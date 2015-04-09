62b2dad2c08ce27850676d83f542a1ee9571d33d
David Warburton <david.warburton@intel.com>
2015-04-09 19:21:41 -0400

    flesh out the handler

--- iotivity_nodejs.cpp
+++ iotivity_nodejs.cpp
@@ -11,8 +11,6 @@
 
 using namespace v8;
 
-
-
 static uv_mutex_t s_callbackQueueMutex;
 static uv_async_t s_notifyJs;
 static std::queue<CsdkWrapper::EntityHandlerInfo *> s_callbackQueue;
@@ -22,35 +20,93 @@ static std::string gParams[CsdkWrapper::NUM_PARAMS];
 
 CsdkWrapper::EntityHandlerResult entityHandlerCallback(CsdkWrapper::EntityHandlerInfo *request)
 {
-
+  /*
+   * Make a copy of request.  When this callback returns iotivity will release the memory
+   * that request points to.
+   */
   CsdkWrapper::EntityHandlerInfo *queRequest = new CsdkWrapper::EntityHandlerInfo(*request);
   for (unsigned int i = 0; i < CsdkWrapper::NUM_PARAMS; i++)
   {
     queRequest->params[i] = gParams[i];
   }
+
+  /*
+   * Save our copy in a queue to be processed by the main loop.  Control access to the queue
+   * with our libuv mutex dedicated to this purpose.
+   */
   uv_mutex_lock(&s_callbackQueueMutex);
   s_callbackQueue.push(queRequest);
   uv_mutex_unlock(&s_callbackQueueMutex);
 
+  /*
+   * Notify the libuv main loop that there is work to be done and it needs to call our handler.
+   */
   uv_async_send(&s_notifyJs);
 
   return CsdkWrapper::EH_RESULT_OK;
 }
+void pushUp(CsdkWrapper::EntityHandlerInfo *cbev)
+{
+  /*
+   * Declare JS objects.  We have one Object and one Array.
+   */
 
+  Local<Object> data = NanNew<Object>();
+  Local<Object> params = NanNew<Array>();
+
+  /*
+   * Fill our array with params, these are the request params from iotivity.
+   */
+  for (int i = 0; i < CsdkWrapper::NUM_PARAMS; i++)
+  {
+    params->Set(i, NanNew(toString(cbev.params[i])));
+  }
+
+  /*
+   * Set the properties of the Object.
+   */
+
+  data->Set(NanNew("resource"), NanNew(cbev->resource));
+  data->Set(NanNew("method"), NanNew(cbev->method));
+  data->Set(NanNew("params"), params);
+
+  /*
+   * Execute the callback with our data object as the parameter.
+   */
+  Local<Value> argv[1] = { data };
+  cb->Call(1, argv);
+}
+
+/*
+ * This function runs on the main event loop of libuv.
+ */
 void notifyJsNow(uv_async_t *handle, int /*status UNUSED*/)
 {
-    uv_mutex_lock(&s_callbackQueueMutex);
-    while(!s_callbackQueue.empty())
-    {
-        auto *cbev = s_callbackQueue.front();
-        uv_mutex_unlock(&s_callbackQueueMutex);
-        // TODO: Propigate event data to JavaScript
-        //cbev->executeCallback();
-        uv_mutex_lock(&s_callbackQueueMutex);
-        s_callbackQueue.pop();
-        delete cbev;
-    }
+
+  /*
+   * lock the queue mutex
+   */
+  uv_mutex_lock(&s_callbackQueueMutex);
+  while(!s_callbackQueue.empty())
+  {
+    /*
+     * Process one item off the queue.  The queue will be unlocked while this happens
+     * because this call will go back up in JS world could take a long time to return
+     * and we want other events to be able to enter the queue while this happens.
+     */
+    auto *cbev = s_callbackQueue.front();
     uv_mutex_unlock(&s_callbackQueueMutex);
+    pushUp(cbev);
+    uv_mutex_lock(&s_callbackQueueMutex);
+
+    /*
+     * Clean up this item.  Need to relock this because the 3 out of the next 4
+     * statements are queue operations (pop, empty, front).
+     */
+    s_callbackQueue.pop();
+    delete cbev;
+  }
+  uv_mutex_unlock(&s_callbackQueueMutex);
 }
 
 NAN_METHOD(version) {
