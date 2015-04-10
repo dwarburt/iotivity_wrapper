5d1a4fc6b421f9e105a71907cbd89468392f58e8
David Warburton <david.warburton@intel.com>
2015-04-10 19:27:38 -0400

    don't try to start iotivity in our main

--- main.js
+++ main.js
@@ -1,12 +1,2 @@
 iot = require("bindings")("iotivity_wrapper_native");
 module.exports = iot;
-
-function handleRequest(req) {
-  console.log("Got a request for: " + req.resource);
-  req.respond();
-}
-
-console.log("Booting iotivity");
-
-iot.start(handleRequest);
-
